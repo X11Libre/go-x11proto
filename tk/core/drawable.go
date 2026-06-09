91cfe5487b423091de9e57f23bc3adef08d15d9b
Enrico Weigelt, metux IT consult <info@metux.net>
2026-06-09 17:41:46 +0200

    tk: Drawable: add PutImage()
    Signed-off-by: Enrico Weigelt, metux IT consult <info@metux.net>

--- tk/core/drawable.go
+++ tk/core/drawable.go
@@ -30,3 +30,7 @@ func (d Drawable) FillRects(gc base.GC, rects []base.Rectangle) error {
 func (d Drawable) PutText8(gc base.GC, x base.INT16, y base.INT16, text string) error {
 	return rpc.PutText8(d.Conn.X11Conn, d.XID, gc, x, y, text)
 }
+
+func (d Drawable) PutImage(gc base.GC, format base.CARD8, depth base.CARD8, width base.CARD16, height base.CARD16, data []byte) error {
+	return rpc.PutImage(d.Conn.X11Conn, d.XID, gc, format, depth, width, height, 0, 0, data)
+}
